3918f78daf51214bb65dd60f1f0ee9ce419cf93d
Flávio Filho <flavio.vezono@ufu.br>
2023-04-25 13:24:10 -0300

    routes fck

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'https://flavio-api-bkpi.herokuapp.com',
     headers: {
         'Content-Type': 'application/json'
       }
